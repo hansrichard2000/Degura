93bfc0cfba5fa42d1eed678a41040986cf5152d2
hansrichard2000 <hansrichard2000@gmail.com>
2023-03-12 11:39:19 +0700

    feat: add UI for Splash Screen

---
+++ app/src/main/java/com/uc/degura/util/SharedPreferenceHelper.java
@@ -0,0 +1,14 @@
+package com.uc.degura.util;
+
+import android.content.SharedPreferences;
+
+public class SharedPreferenceHelper {
+
+//    private static final String PREFS = "pref";
+//
+//    private static SharedPreferenceHelper instance;
+//
+//    private SharedPreferences prefs;
+
+
+}
