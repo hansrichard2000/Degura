ad8355191458bd54893a9423bc0a06b70f02f5d9
hansrichard2000 <hansrichard2000@gmail.com>
2023-03-12 22:01:38 +0700

    feat: add code for access camera and gallery

--- app/src/main/java/com/uc/degura/view/main/FishEyeFragment.java
+++ app/src/main/java/com/uc/degura/view/main/FishEyeFragment.java
@@ -1,60 +1,43 @@
 package com.uc.degura.view.main;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.uc.degura.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FishEyeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class FishEyeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    @BindView(R.id.btn_camera_eye)
+    Button btn_camera_eye;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    @BindView(R.id.btn_gallery_eye)
+    Button btn_gallery_eye;
+
+    private static final String TAG = "FishEyeFragment";
 
     public FishEyeFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FishEyeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FishEyeFragment newInstance(String param1, String param2) {
-        FishEyeFragment fragment = new FishEyeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -62,5 +45,47 @@ public class FishEyeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_fish_eye, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this, view);
+
+        ActivityResultLauncher<Intent> cameraResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // There are no request codes
+                        Intent data = result.getData();
+//                            doSomeOperations();
+                    }
+                }
+        );
+
+        btn_camera_eye.setOnClickListener(v -> {
+            if (getActivity().checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                cameraResultLauncher.launch(cameraIntent);
+            } else {
+                getActivity().requestPermissions(new String[]{Manifest.permission.CAMERA}, 100);
+            }
+        });
+
+        ActivityResultLauncher<Intent> galleryResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // There are no request codes
+                        Intent data = result.getData();
+//                            doSomeOperations();
+                    }
+                }
+        );
+
+        btn_gallery_eye.setOnClickListener(v -> {
+            Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            galleryResultLauncher.launch(galleryIntent);
+        });
     }
 }
