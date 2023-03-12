c5cea83b733a28aac0ab80fb015090b2e7fb589c
hansrichard2000 <hansrichard2000@gmail.com>
2023-03-12 18:10:35 +0700

    churn: add additional dependency for viewpager, image, and fragment elements

--- app/src/main/java/com/uc/degura/view/result/ResultsFragment.java
+++ app/src/main/java/com/uc/degura/view/result/ResultsFragment.java
@@ -61,6 +61,6 @@ public class ResultsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_image_result, container, false);
+        return inflater.inflate(R.layout.fragment_results, container, false);
     }
 }
